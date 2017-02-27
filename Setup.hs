c3fb25115eedc07a336a4a41dbdb57a6b13fd675
TheBizzle <jason.bertsche@gmail.com>
2017-02-26 18:37:57 -0600

    Initial commit

---
+++ Setup.hs
@@ -0,0 +1,9 @@
+import Distribution.PackageDescription
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+
+import System.Process
+
+main :: IO ()
+main = defaultMain
