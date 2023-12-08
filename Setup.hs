f26ec78b5896901b0ee2d4ba724ec8039ee71f58
TheBizzle <jason.bertsche@gmail.com>
2023-12-07 18:06:56 -0600

    Delete: Useless 'Setup.hs'

--- Setup.hs
+++
@@ -1,9 +0,0 @@
-import Distribution.PackageDescription
-import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Setup
-
-import System.Process
-
-main :: IO ()
-main = defaultMain
