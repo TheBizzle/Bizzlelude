7a8206c7353dfa6c7b400e5bf9494302c5de9330
TheBizzle <jason.bertsche@gmail.com>
2025-12-12 21:05:24 -0600

    Feature: Add a bunch of built-ins

--- src/main/External.hs
+++ src/main/External.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-module External(module Control.Arrow, module Control.Applicative, module Control.Monad, module Control.Monad.IO.Class, module Data.Bifunctor, module Data.Bool, module Data.Char, module Data.Either, module Data.Eq, module Data.Foldable, module Data.Function, module Data.Functor, module Data.Int, module Data.IntSet, module Data.Map, module Data.Maybe, module Data.Monoid, module Data.Ord, module Data.Semigroup, module Data.Set, module Data.Text, module Data.Traversable, module Data.Tuple, module Debug.Trace, module GHC.Base, module GHC.Enum, module GHC.Err, module GHC.Float, module GHC.IO, module GHC.Num, module GHC.Real, module GHC.Show, module Numeric, module System.IO.Error, module Text.Read) where
+module External(module Control.Arrow, module Control.Applicative, module Control.Monad, module Control.Monad.IO.Class, module Control.Monad.State, module Data.Bifoldable, module Data.Bifunctor, module Data.Bool, module Data.ByteString, module Data.Char, module Data.Either, module Data.Eq, module Data.Foldable, module Data.Function, module Data.Functor, module Data.Int, module Data.IntSet, module Data.List.NonEmpty, module Data.Map, module Data.Maybe, module Data.Monoid, module Data.Ord, module Data.Semigroup, module Data.Set, module Data.Text, module Data.Traversable, module Data.Tuple, module Data.Validation, module Data.Void, module Data.Word, module Debug.Trace, module GHC.Base, module GHC.Enum, module GHC.Err, module GHC.Float, module GHC.IO, module GHC.Num, module GHC.Real, module GHC.Show, module Numeric, module System.IO.Error, module Text.Read) where
 
 import Control.Arrow((&&&), (***))
-import Control.Applicative(Alternative((<|>)), Applicative((<*>), (<*), (*>), pure))
+import Control.Applicative((<**>), Alternative((<|>), empty, many, some), Applicative((<*>), (<*), (*>), pure), liftA2, optional)
 import Control.Monad((=<<), filterM, foldM, foldM_, forM, forM_, guard, join, mapM, mapM_, Monad((>>), (>>=), return), MonadPlus(), sequence, sequence_, unless, when)
 import Control.Monad.IO.Class(liftIO)
+import Control.Monad.State(evalState, execState, mapState, runState, State, withState)
 
+import Data.Bifoldable(Bifoldable, bifold, bifoldMap, bimapM_)
 import Data.Bifunctor(Bifunctor(bimap, first, second))
 import Data.Bool(Bool(False, True), (&&), (||), not, otherwise)
+import Data.ByteString(ByteString)
 import Data.Char(Char, digitToInt, intToDigit)
 import Data.Either(Either(Left, Right), either, lefts, isLeft, isRight, partitionEithers, rights)
 import Data.Eq(Eq((==), (/=)))
@@ -16,6 +19,7 @@ import Data.Function(($), (.), const, flip, id, on)
 import Data.Functor((<$), (<$>), ($>), Functor(fmap), void)
 import Data.Int(Int, Int8, Int16, Int32, Int64)
 import Data.IntSet(IntSet)
+import Data.List.NonEmpty(nonEmpty, NonEmpty)
 import Data.Map(Map)
 import Data.Maybe(catMaybes, fromMaybe, isJust, isNothing, listToMaybe, maybe, maybeToList, Maybe(Just, Nothing))
 import Data.Monoid(Monoid(mappend, mempty))
@@ -23,8 +27,11 @@ import Data.Ord(Ord((<), (<=), (>), (>=), compare, max, min), Ordering(EQ, GT, L
 import Data.Semigroup(Semigroup((<>)))
 import Data.Set(Set)
 import Data.Text(lines, Text, unlines, unwords, words)
-import Data.Traversable(traverse)
+import Data.Traversable(for, sequenceA, Traversable, traverse)
 import Data.Tuple(curry, fst, snd, swap, uncurry)
+import Data.Word(Word, Word8, Word16, Word32, Word64)
+import Data.Validation(bindValidation, validation, Validation(Failure, Success))
+import Data.Void(Void)
 
 import Debug.Trace(trace, traceEvent, traceEventIO, traceId, traceIO, traceM, traceMarker, traceMarkerIO, traceShow, traceShowId, traceShowM, traceStack)
 
@@ -42,4 +49,4 @@ import Numeric(Floating, pi, exp, log, sqrt, (**), logBase, sin, cos, tan, asin,
 
 import System.IO.Error(IOError, ioError, userError)
 
-import Text.Read(read)
+import Text.Read(read, readMaybe)
